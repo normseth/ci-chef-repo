53a92b92bf88679e5e7ec95f91ab8759be82ba9d
Nik Ormseth <nik@ormseth.net>
2014-08-27 15:54:00 -0700

    stubs for using chef-zero on a build slave

---
+++ .chef/knife.rb
@@ -0,0 +1,12 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "normseth"
+client_key               "#{current_dir}/dummy.pem"
+validation_client_name   "dummy-validator"
+validation_key           "#{current_dir}/dummy-validator.pem"
+chef_server_url          "http://192.168.43.3:8889"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
