49cdff6f5225575848fac19df55e07b6c716a736
Nik Ormseth <nik@ormseth.net>
2014-09-03 15:57:59 -0700

    added to cookbooks path for vendoring with berkshelf

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -8,5 +8,6 @@ validation_key           "#{current_dir}/dummy-validator.pem"
 chef_server_url          "http://#{ENV['CHEF_SERVER_IP']}:8889"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            ["#{current_dir}/../cookbooks"]
+#cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            ["#{current_dir}/../cookbooks" , "#{current_dir}/../berks-cookbooks" ]
 
