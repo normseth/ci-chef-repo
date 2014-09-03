eae0a5ff54997c4917a9d81fff4b828a1cd26f85
Nik Ormseth <nik@ormseth.net>
2014-09-03 13:52:18 -0700

    changed server url to environment variable

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ node_name                "normseth"
 client_key               "#{current_dir}/dummy.pem"
 validation_client_name   "dummy-validator"
 validation_key           "#{current_dir}/dummy-validator.pem"
-chef_server_url          "http://192.168.43.3:8889"
+chef_server_url          ENV['CHEF_SERVER_URL'} || "http://192.168.43.1:8889"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
