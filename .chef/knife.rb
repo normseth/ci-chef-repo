44e8f664121c55537d0dcdb5c1e63f4971d1fcda
Nik Ormseth <nik@ormseth.net>
2014-09-03 15:53:58 -0700

    fixing environment for chef server

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ node_name                "normseth"
 client_key               "#{current_dir}/dummy.pem"
 validation_client_name   "dummy-validator"
 validation_key           "#{current_dir}/dummy-validator.pem"
-chef_server_url          ENV['CHEF_SERVER_URL']
+chef_server_url          "http://#{ENV['CHEF_SERVER_IP']}:8889"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
