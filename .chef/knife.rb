9ea2de1de7c590a124da7c1c11e00b2fd2b7b474
Nik Ormseth <nik@ormseth.net>
2014-09-28 07:36:12 -0700

    cleaning up

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,13 +1,24 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "normseth"
-client_key               "#{current_dir}/dummy.pem"
-validation_client_name   "dummy-validator"
-validation_key           "#{current_dir}/dummy-validator.pem"
-chef_server_url          "http://#{ENV['CHEF_SERVER_IP']}:8889"
+client_key               "#{current_dir}/normseth.pem"
+validation_client_name   "demo-v2-validator"
+validation_key           "#{current_dir}/demo-v2-validator.pem"
+if ENV['CHEF_SERVER_IP'] then
+  chef_server_url          "http://#{ENV['CHEF_SERVER_IP']}:8889"
+else
+  chef_server_url          "https://api.opscode.com/organizations/demo-v2"
+end
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-#cookbook_path            ["#{current_dir}/../cookbooks"]
-cookbook_path            ["#{current_dir}/../cookbooks" , "#{current_dir}/../berks-cookbooks" ]
+cookbook_path            [ "#{current_dir}/../cookbooks", "#{current_dir}/../..cookbooks" ]
 
+cookbook_copyright      "Level 11"
+cookbook_license        "All rights reserved"
+cookbook_email          "nikormseth@level11.com"
+
+# Encyption key for data bags
+#knife[:secret_file] = "#{current_dir}/encrypted_data_bag_secret"
